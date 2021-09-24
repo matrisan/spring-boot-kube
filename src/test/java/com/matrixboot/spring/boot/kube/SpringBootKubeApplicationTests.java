512f6a1d3bd462e341abfbe7ac93047b8f75af37
shishaodong <spring-boot@qq.com>
2021-09-24 19:49:17 +0800

    Initial commit

---
+++ src/test/java/com/matrixboot/spring/boot/kube/SpringBootKubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matrixboot.spring.boot.kube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
