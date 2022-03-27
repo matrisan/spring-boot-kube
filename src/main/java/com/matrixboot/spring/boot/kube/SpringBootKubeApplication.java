3e73e7510549617ab06ddc9a62bd7253cb926f8a
shishaodong <spring-boot@qq.com>
2022-03-27 20:17:17 +0800

    去除 自定义注解

--- src/main/java/com/matrixboot/spring/boot/kube/SpringBootKubeApplication.java
+++ src/main/java/com/matrixboot/spring/boot/kube/SpringBootKubeApplication.java
@@ -1,6 +1,5 @@
 package com.matrixboot.spring.boot.kube;
 
-import com.matrixboot.redis.annotation.EnableRedisStreamClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -8,7 +7,6 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 /**
  * @author shishaodong
  */
-@EnableRedisStreamClients
 @EnableAspectJAutoProxy
 @SpringBootApplication
 public class SpringBootKubeApplication {
--- src/main/java/com/matrixboot/spring/boot/kube/interfaces/facade/IdempotentFacade.java
+++ src/main/java/com/matrixboot/spring/boot/kube/interfaces/facade/IdempotentFacade.java
@@ -1,14 +1,9 @@
 package com.matrixboot.spring.boot.kube.interfaces.facade;
 
-import com.matrixboot.idempotent.annotation.Idempotent;
-import com.matrixboot.spring.boot.kube.infrastructure.remote.IRedisStreamInput;
 import com.matrixboot.spring.boot.kube.interfaces.dto.IdempotentCommand;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.Resource;
-import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -20,18 +15,13 @@ import java.util.concurrent.TimeUnit;
 @RestController
 public class IdempotentFacade {
 
-    @Resource
-    private IRedisStreamInput redisStreamInput;
 
     @PostMapping("idempotent1")
-    @Idempotent(value = "#command.username")
     public IdempotentCommand idempotent1(@RequestBody IdempotentCommand command) {
-        redisStreamInput.stream(command);
         return command;
     }
 
     @PostMapping("idempotent2")
-    @Idempotent(value = "@idempotentService.convert(#command)", timeout = 30, unit = TimeUnit.SECONDS)
     public IdempotentCommand idempotent2(@RequestBody IdempotentCommand command) {
         return command;
     }
